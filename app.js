2e39d7160f405a7660720aa8b8b65fef7d7ad37e
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2023-07-25 18:31:09 +0530

    Update puzzle solver

--- app.js
+++ app.js
@@ -23,3 +23,24 @@ function setValue(max, min) {
 //to select 2 random grids 
 setValue(5, 1);
 setValue(10, 5);
+
+function inputValidate(event){
+  if (event.key === "Enter"){
+    event.preventDefault();
+    if (selctedValues.includes(parseInt(event.target.value))){
+      alert("The entered value is already in the grid. Please select a different value")
+    }
+    else{
+      selctedValues.push(parseInt(event.target.value))
+    }
+  }
+}
+
+function sudokuValidate(){
+  if (selctedValues.length==9){
+    alert("You have solved the puzzle!");
+    location.reload();
+  } else {
+    alert("The puzzle is not solved yet! Please complete it before submitting");
+  }
+}

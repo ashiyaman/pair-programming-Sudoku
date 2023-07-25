c3d83e42800d2855da3d2fbd392eaf7cb3658054
Ashiya Banu <ashiya1005@gmail.com>
2023-07-25 23:12:23 +0530

    change css

--- app.js
+++ app.js
@@ -7,12 +7,13 @@ function setValue(max, min) {
   let temp = Math.floor((Math.random() * (10 - 1) + 1));
   let tempGrid = Math.floor((Math.random() * (max - min) + min));
   let tempDiv = document.querySelector("#grid" + tempGrid);
+  tempDiv.disabled = true;
 
   //check whether the value is already present or not
   while (selctedValues.includes(temp)) {
     temp = Math.floor((Math.random() * (10 - 1) + 1));
   }
-  tempDiv.innerHTML = temp;
+  tempDiv.value = temp;
 
   //store the values to the array
   selctedValues.push(temp);
@@ -24,20 +25,20 @@ function setValue(max, min) {
 setValue(5, 1);
 setValue(10, 5);
 
-function inputValidate(event){
-  if (event.key === "Enter"){
+function inputValidate(event) {
+  if (event.key === "Enter") {
     event.preventDefault();
-    if (selctedValues.includes(parseInt(event.target.value))){
+    if (selctedValues.includes(parseInt(event.target.value))) {
       alert("The entered value is already in the grid. Please select a different value")
     }
-    else{
+    else {
       selctedValues.push(parseInt(event.target.value))
     }
   }
 }
 
-function sudokuValidate(){
-  if (selctedValues.length==9){
+function sudokuValidate() {
+  if (selctedValues.length == 9) {
     alert("You have solved the puzzle!");
     location.reload();
   } else {
